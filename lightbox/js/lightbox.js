81dff03323bbe56aa8098a7c2418b8cc8bd19447
Juanbohorquezz <jpablobohorquez@gmail.com>
2023-03-02 22:41:16 -0500

    gallery

--- lightbox/js/lightbox.js
+++ lightbox/js/lightbox.js
@@ -3,13 +3,14 @@ let lightboxBackground = document.getElementById ("lightboxBackground");
 let lightbox = document.getElementById ("lightbox");
 let lightboxCloser = document.getElementById("lightboxCloser");
 
+//gallery1
 function openLightbox(){
     lightboxContainer.classList.add("display");
     lightbox.src = this.src;
 }
 
-let coffeeImage = document.getElementById ("coffeeImage");
-    coffeeImage.onclick = openLightbox;
+let gallery1 = document.getElementById ("gallery1");
+    gallery1.onclick = openLightbox;
 
 function closeLightbox(){
     lightboxContainer.classList.remove("display");
@@ -19,14 +20,65 @@ function closeLightbox(){
 lightboxBackground.onclick = closeLightbox;
 lightboxCloser.onclick = closeLightbox;
 
-
+//gallery2
 function openLightbox(){
     lightboxContainer.classList.add("display");
     lightbox.src = this.src;
 }
 
-let coffeeImage2 = document.getElementById ("coffeeImage2");
-    coffeeImage2.onclick = openLightbox;
+let gallery2 = document.getElementById ("gallery2");
+    gallery2.onclick = openLightbox;
+
+function closeLightbox(){
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "" ;
+}
+
+lightboxBackground.onclick = closeLightbox;
+lightboxCloser.onclick = closeLightbox;
+
+//gallery3
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let gallery3 = document.getElementById ("gallery3");
+    gallery3.onclick = openLightbox;
+
+function closeLightbox(){
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "" ;
+}
+
+lightboxBackground.onclick = closeLightbox;
+lightboxCloser.onclick = closeLightbox;
+
+//gallery4
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let gallery4 = document.getElementById ("gallery4");
+    gallery4.onclick = openLightbox;
+
+function closeLightbox(){
+    lightboxContainer.classList.remove("display");
+    lightbox.src = "" ;
+}
+
+lightboxBackground.onclick = closeLightbox;
+lightboxCloser.onclick = closeLightbox;
+
+//gallery5
+function openLightbox(){
+    lightboxContainer.classList.add("display");
+    lightbox.src = this.src;
+}
+
+let gallery5 = document.getElementById ("gallery5");
+    gallery5.onclick = openLightbox;
 
 function closeLightbox(){
     lightboxContainer.classList.remove("display");
